79c9617034f5594771c6a34be57b8737cf74ea50
Eóin Martin <eoinmartin3007@gmail.com>
2014-09-23 22:41:17 +0100

    Test.js format update

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var Reddit = require('./node-reddit');
 var reddit = new Reddit('72l8gcTRgRbZotsejYJOhNNzd9A');
 
-console.log(reddit);
-
-reddit.u().then(function(res) {
+reddit
+	.u()
+	.then(function(res) {
 		console.log(res);
 	}).catch(function(err){
 		console.error(err);
