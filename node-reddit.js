7922cc8cb5102a31cd7fcabe19a94061f10b7c01
Eóin Martin <eoinmartin3007@gmail.com>
2014-09-24 20:57:29 +0100

    Subs method

--- node-reddit.js
+++ node-reddit.js
@@ -12,12 +12,18 @@ var Reddit = function(token) {
 Reddit.prototype.setToken = function(token) {
 	this.TOKEN = token;
 	agent.setToken(token);
-}
+};
 
 Reddit.prototype.u = function(username) {
 	var defer = Q.defer();
 	if(!username) return agent.identity.me();
 	return agent.identity.me();
+};
+
+Reddit.prototype.r = function(sub) {
+	var defer = Q.defer();
+	if(!sub) return agent.mysubbreddits.subscriber();
+	return agent.mysubbreddits.subscriber();
 }
 
 module.exports = Reddit;
